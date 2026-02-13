71d2f3013d82e2f7c8f285416bb9998a1f11b12b
bavonsamedi-web <bavonsamedi@gmail.com>
2026-02-13 21:08:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,40 @@
 import os
 from fastapi import FastAPI, Request
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    MessageHandler,
+    filters,
+    ContextTypes,
+)
 
-TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+WEBHOOK_URL = os.getenv("WEBHOOK")
 
 app = FastAPI()
+application = Application.builder().token(BOT_TOKEN).build()
 
-application = ApplicationBuilder().token(TOKEN).build()
 
+# --- команды ---
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Бот работает 🔥")
+    await update.message.reply_text("Я жив. И теперь я мощный 😈")
+
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(update.message.text)
+
 
 application.add_handler(CommandHandler("start", start))
+application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
 
 @app.on_event("startup")
-async def startup():
+async def on_startup():
     await application.initialize()
     await application.start()
+    await application.bot.set_webhook(WEBHOOK_URL)
+
 
 @app.post("/")
 async def webhook(request: Request):
